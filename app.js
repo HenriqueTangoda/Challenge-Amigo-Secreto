dbfe18d65fb8c77278926020981598c8aa424c32
Henrique Tangoda <henriquetangodabaia@gmail.com>
2025-02-19 21:48:18 -0300

    Criando lista para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+let campoDeEntrada = document.getElementById('amigo').value.trim();
+
+ if (campoDeEntrada === '') {
+    alert("Por favor, insira um nome.");
+
+ } else {
+    listaAmigos.push(campoDeEntrada);
+    document.getElementById('amigo').value = ''
+    console.log(listaAmigos);
+ }
+}
