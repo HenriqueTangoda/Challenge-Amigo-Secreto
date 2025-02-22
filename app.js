0ae234e91e6b895a9105c622ce60ca74b0f69595
Henrique Tangoda <henriquetangodabaia@gmail.com>
2025-02-22 15:32:40 -0300

    Criando função para sortear os amigos

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ let campoDeEntrada = document.getElementById("amigo").value.trim();
 
  if (campoDeEntrada === "") {
     alert("Por favor, insira um nome.");
-
  } else {
    listaAmigos.push(campoDeEntrada);
     document.getElementById("amigo").value = "";
@@ -24,4 +23,21 @@ for (let i = 0; i < listaAmigos.length; i++) {
    item.textContent = listaAmigos[i];
    listagem.appendChild(item);
 }
+}
+
+//FUNÇÃO PARA SORTEAR OS NOMES NA LISTA
+function sortearAmigo() {
+   if (listaAmigos.length === 0) {
+      alert("Insira alguns nomes para sortear");
+   } else {
+      let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+      let amigoAleatorio = listaAmigos[indiceAleatorio];
+      atualizarSorteado(amigoAleatorio);
+   }
+}
+
+//FUNÇÃO ATUALIZAR O AMIGO SORTEADO
+function atualizarSorteado(amigoAleatorio) {
+   let sorteado = document.getElementById("resultado");
+   sorteado.innerHTML = (`Seu amigo sorteado é ${amigoAleatorio}`)
 }
