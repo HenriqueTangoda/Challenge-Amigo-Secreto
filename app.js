271814b48de9c20ce205f10073a697946833895c
Henrique Tangoda <henriquetangodabaia@gmail.com>
2025-02-22 14:10:49 -0300

    Criando função para exibição de listagem

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//LISTA DE AMIGOS
 let listaAmigos = [];
 
+//FUNÇÃO PARA ADICIONAR NOMES VALIDOS A LISTA
 function adicionarAmigo() {
-let campoDeEntrada = document.getElementById('amigo').value.trim();
+let campoDeEntrada = document.getElementById("amigo").value.trim();
 
- if (campoDeEntrada === '') {
+ if (campoDeEntrada === "") {
     alert("Por favor, insira um nome.");
 
  } else {
-    listaAmigos.push(campoDeEntrada);
-    document.getElementById('amigo').value = ''
-    console.log(listaAmigos);
+   listaAmigos.push(campoDeEntrada);
+    document.getElementById("amigo").value = "";
+    atualizarListaDeAmigos();
  }
+}
+
+//FUNÇÃO PARA ATUALIZAR A LISTA DE NOMES
+function atualizarListaDeAmigos() {
+let listagem = document.getElementById("listaAmigos");
+listagem.innerHTML = "";
+for (let i = 0; i < listaAmigos.length; i++) {
+   let item = document.createElement("li");
+   item.textContent = listaAmigos[i];
+   listagem.appendChild(item);
+}
 }
